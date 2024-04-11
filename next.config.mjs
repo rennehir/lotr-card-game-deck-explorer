4fe3b66492c859e43d3d12d280a791ac6d74c750
rennehir <renne@rennehir.fi>
2024-04-11 16:34:05 +0300

    Add ringsdb.com to known image hosts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ringsdb.com",
+        pathname: "/bundles/cards/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
