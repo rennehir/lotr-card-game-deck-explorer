308e74ef2c9f32dd352a1d479d6532b1218b2a87
rennehir <renne@rennehir.fi>
2024-04-12 14:51:50 +0300

    Add error handling to search

--- src/components/deck-search.tsx
+++ src/components/deck-search.tsx
@@ -4,6 +4,7 @@ import { useState, useTransition } from "react";
 import { searchDeck } from "@/app/actions";
 import { Deck } from "@/lib/ringsdb";
 import {
+  Alert,
   Button,
   Heading,
   HStack,
@@ -15,24 +16,32 @@ import { DeckPreview } from "./deck-preview";
 
 export const DeckSearch = () => {
   const [deck, setDeck] = useState<Deck>();
+  const [error, setError] = useState<string>();
   const [isPending, startTransition] = useTransition();
+
+  const handleSearch = async (formData: FormData) => {
+    startTransition(async () => {
+      setError(undefined);
+      try {
+        const deck = await searchDeck(formData);
+        setDeck(deck);
+      } catch (error) {
+        setError("Failed to find a deck");
+        setDeck(undefined);
+      }
+    });
+  };
 
   return (
     <VStack spacing={6} marginBlock={6} align="stretch">
       <Heading as="h2">Search for a deck</Heading>
-      <form
-        action={async (formData) => {
-          startTransition(async () => {
-            const deck = await searchDeck(formData);
-            setDeck(deck);
-          });
-        }}
-      >
+      <form action={handleSearch}>
         <HStack spacing={2}>
           <Input name="deckId" type="text" placeholder="Search for a deck" />
           <Button type="submit">Search</Button>
         </HStack>
       </form>
+      {error && <Alert status="error">{error}</Alert>}
 
       {(deck || isPending) && (
         <Skeleton isLoaded={deck && !isPending}>
